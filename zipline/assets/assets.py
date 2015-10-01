3c55d8e26c760ee4d3e391f003ffb3fc954e26e9
warren-oneill <warrenon@gmail.com>
2015-10-01 16:02:34 +0200

    BUG: adds future_lookup to symbol function when as_of is given

--- zipline/assets/assets.py
+++ zipline/assets/assets.py
@@ -275,7 +275,7 @@ class AssetFinder(object):
                 ).scalar()
                 if sid is not None:
                     return self._retrieve_equity(sid)
-
+            self.lookup_future(symbol)
             raise SymbolNotFound(symbol=symbol)
 
         else:
@@ -295,7 +295,6 @@ class AssetFinder(object):
             if len(sids) == 1:
                 return self._retrieve_equity(sids[0]['sid'])
             elif not sids:
-                self.lookup_future(symbol)
                 raise SymbolNotFound(symbol=symbol)
             else:
                 raise MultipleSymbolsFound(
